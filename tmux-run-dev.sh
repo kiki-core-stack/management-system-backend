8de30ece1f04e54f73d52a484209618aa7572f67
kiki-kanri <a470666@gmail.com>
2024-08-14 10:05:53 +0800

    chore: update `tmux-run-dev.sh` script

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -4,5 +4,5 @@ name="el-plus-admin-backend"
 
 if ! tmux ls | grep -q "^$name:"; then
 	tmux new-session -ds $name
-	tmux send-keys -t $name 'bun --bun run dev' C-m
+	tmux send-keys -t $name 'pnpm run dev' C-m
 fi
