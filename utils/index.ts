9572e3cc1e89c707ba466b165679f0dd0ea3c5ee
kiki-kanri <a470666@gmail.com>
2024-08-16 21:50:04 +0800

    feat: add global common utils

---
+++ utils/index.ts
@@ -0,0 +1,4 @@
+export { commonAjvValidatorSchemas } from '@kikiutils/el-plus-admin-pack/constants/validator';
+export * from '@kikiutils/el-plus-admin-pack/utils/nitropack';
+export { requireTwoFactorAuthentication } from '@kikiutils/el-plus-admin-pack/utils/two-factor-authentication';
+export { getEnumNumberValues, getEnumStringValues } from '@kikiutils/node/enum';
