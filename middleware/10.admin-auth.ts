4bdaa9feed0b847fe8c99231c9948e575dc4768c
kiki-kanri <a470666@gmail.com>
2024-08-16 22:14:38 +0800

    feat: add admin auth middleware

---
+++ middleware/10.admin-auth.ts
@@ -0,0 +1,21 @@
+import { AdminModel } from '@kikiutils/el-plus-admin-pack/models';
+
+const allowNoAuthPaths = new Set([
+	'/api/admin/auth/login',
+	'/api/admin/auth/otp/email/send',
+	'/api/admin/info',
+	'/api/ver-code'
+]);
+
+export default defineEventHandler(async (event) => {
+	if (!event.path.startsWith('/api')) return;
+	if (event.context.session.adminId) {
+		event.context.admin = await AdminModel.findById(event.context.session.adminId);
+		if (!event.context.admin?.enabled) delete event.context.session.adminId;
+	}
+
+	if (!event.context.session.adminId) {
+		const qMarkIndex = event.path.indexOf('?');
+		if (!allowNoAuthPaths.has(qMarkIndex === -1 ? event.path : event.path.substring(0, qMarkIndex))) createH3ErrorAndThrow(401);
+	}
+});
