2675f216301377d30930242dbf63b47590a2f9b3
kiki-kanri <a470666@gmail.com>
2024-08-16 22:20:35 +0800

    feat: add response utils and required types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,14 @@
 export type {} from '@kikiutils/el-plus-admin-pack/types';
 export type {} from '@kikiutils/el-plus-admin-pack/types/data';
 export type {} from '@kikiutils/el-plus-admin-pack/types/mongoose';
+
+declare global {
+	interface ProcessedApiRequestQueries {
+		filterQuery: Dict<any>;
+		limit: number;
+		offset: number;
+		page: number;
+		selectFields: string[];
+		skip: number;
+	}
+}
