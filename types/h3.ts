0975fffd5846df7e1c44e6681de8c06c6513b8f6
kiki-kanri <a470666@gmail.com>
2024-08-16 22:08:51 +0800

    feat: update session and define h3 event.context types

--- types/h3.ts
+++ types/h3.ts
@@ -1,4 +1,11 @@
+import type { AdminDocument } from '@kikiutils/el-plus-admin-pack/models';
 import type { H3Event } from 'h3';
+
+declare module 'h3' {
+	interface H3EventContext {
+		admin?: Nullable<AdminDocument>;
+	}
+}
 
 declare global {
 	type H3RequestEvent = H3Event;
--- types/session.ts
+++ types/session.ts
@@ -1,5 +1,8 @@
 declare module '@kikiutils/nitro-session' {
 	interface H3EventContextSession {
+		adminId: string;
+		tempAdminIdForSendEmailOtpCode: string;
+
 		/**
 		 * Validation codes generated in `/api/ver-code` are stored here.
 		 */
