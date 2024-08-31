418ae84fa0d6cc117efba043cf3a2ae009fc602b
kiki-kanri <a470666@gmail.com>
2024-09-01 03:50:00 +0800

    refactor: remove outer try-catch when using transaction

--- api/admin/[id]/index.delete.ts
+++ api/admin/[id]/index.delete.ts
@@ -1,16 +1,11 @@
 import { AdminLogModel, AdminModel, mongooseConnections } from '@kikiutils/el-plus-admin-pack/models';
 
 export default defineEventHandler(async (event) => {
-	try {
-		return await mongooseConnections.default!.transaction(async (session) => {
-			return await getModelDocumentByRouteIdAndDelete(event, AdminModel, { session }, async (admin) => {
-				if (admin.id === event.context.session.adminId) throw new ApiError(409, '無法刪除自己！');
-				if ((await AdminModel.countDocuments()) === 1) throw new ApiError(409, '無法刪除最後一位管理員！');
-				await AdminLogModel.deleteMany({ admin }, { session });
-			});
+	return await mongooseConnections.default!.transaction(async (session) => {
+		return await getModelDocumentByRouteIdAndDelete(event, AdminModel, { session }, async (admin) => {
+			if (admin.id === event.context.session.adminId) throw new ApiError(409, '無法刪除自己！');
+			if ((await AdminModel.countDocuments()) === 1) throw new ApiError(409, '無法刪除最後一位管理員！');
+			await AdminLogModel.deleteMany({ admin }, { session });
 		});
-	} catch (error) {
-		if (isError(error)) throw error;
-		throw new ApiError();
-	}
+	});
 });
