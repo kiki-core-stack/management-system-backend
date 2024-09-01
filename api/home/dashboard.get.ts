eec14e32e52430e5879b930b9aabc56bb54b331f
kiki-kanri <a470666@gmail.com>
2024-09-01 09:06:28 +0800

    refactor: update homepage data API

--- api/home/dashboard.get.ts
+++ api/home/dashboard.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
+	// @ts-expect-error
 	const processedQueries = getModelToPaginatedResponseDataProcessedQueries(event);
-	processedQueries;
 	return createApiSuccessResponseData({});
 });
