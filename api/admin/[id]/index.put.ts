ad11a07853e1a2b9b104c10e14510f3a852a3d4b
kiki-kanri <a470666@gmail.com>
2024-09-02 15:58:13 +0800

    refactor: update save admin API

--- api/admin/[id]/index.put.ts
+++ api/admin/[id]/index.put.ts
@@ -6,10 +6,9 @@ import { saveAdminDataValidator } from '@/validators/admin';
 
 export default defineEventHandler(async (event) => {
 	const admin = await AdminModel.findByRouteIdOrThrowNotFoundError(event);
-	const data = await saveAdminDataValidator(event);
-	data.enabled = data.enabled || admin.id === event.context.session.adminId;
-	const updateQuery: UpdateQuery<AdminDocument> = data;
-	if (!data.email) updateQuery.$unset = { email: true };
+	const updateQuery: UpdateQuery<AdminDocument> = await saveAdminDataValidator(event);
+	updateQuery.enabled = updateQuery.enabled || admin.id === event.context.session.adminId;
+	if (!updateQuery.email) updateQuery.$unset = { email: true };
 	await admin.updateOne(updateQuery);
 	return createApiSuccessResponseData();
 });
