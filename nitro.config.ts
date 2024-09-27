b65c9751f3132a5729a7047c5884ada068f555f1
kiki-kanri <a470666@gmail.com>
2024-09-27 11:36:12 +0800

    feat: configure error handler

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@
 export default defineNitroConfig({
 	alias: { '@': '~/' },
 	compressPublicAssets: true,
+	errorHandler: '@kikiutils/kiki-core-stack-pack/nitropack/handlers/error',
 	minify: process.env.NODE_ENV !== 'development',
 	noPublicDir: true,
 	serveStatic: false,
