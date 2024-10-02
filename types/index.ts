7605796ada4be2fb23c8df05f764eeb8e57d236b
kiki-kanri <a470666@gmail.com>
2024-10-02 11:26:47 +0800

    chore: update types export file

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+export type {} from '@kikiutils/kiki-core-stack-pack/nitro/types/mongoose';
 export type {} from '@kikiutils/kiki-core-stack-pack/types';
 export type {} from '@kikiutils/kiki-core-stack-pack/types/data';
-export type {} from '@kikiutils/kiki-core-stack-pack/types/mongoose';
+export type {} from '@kikiutils/mongoose/types';
