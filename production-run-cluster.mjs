95e0712ca628d6dc6fb7c923d519c27ded90d454
kiki-kanri <a470666@gmail.com>
2024-10-07 00:35:36 +0800

    fix: correct syntax errors in `production-run-cluster.mjs`

--- production-run-cluster.mjs
+++ production-run-cluster.mjs
@@ -1,21 +1,8 @@
 import cluster from 'cluster';
-import { createServer } from 'http';
-import { cpus } from 'os';
-
-let listener;
-try {
-	listener = await import('./.output/server/index.mjs');
-} catch {
-	try {
-		listener = await import('./index.mjs');
-	} catch {
-		throw new Error('No listener found');
-	}
-}
 
 if (cluster.isPrimary) {
 	console.log(`Primary process ${process.pid} is running`);
-	const clusterWorkers = +process.env.NITRO_CLUSTER_WORKERS || cpus().length;
+	const clusterWorkers = +process.env.NITRO_CLUSTER_WORKERS || (await import('os')).cpus().length;
 	console.log(`Forking ${clusterWorkers} workers`);
 	for (let i = 0; i < clusterWorkers; i++) cluster.fork();
 	cluster.on('exit', (worker, code, signal) => {
@@ -27,9 +14,19 @@ if (cluster.isPrimary) {
 		}, 3000);
 	});
 } else {
+	let listener;
+	try {
+		listener = (await import('./.output/server/index.mjs')).listener;
+	} catch {
+		try {
+			listener = (await import('./index.mjs')).listener;
+		} catch {}
+	}
+
+	if (!listener) throw new Error('No listener found');
 	const listenHost = process.env.NITRO_HOST || '127.0.0.1';
 	const listenPort = process.env.NITRO_PORT || 8000;
-	const server = createServer(listener);
+	const server = (await import('http')).createServer(listener);
 	server.on('error', (error) => console.error(`Server encountered an error: ${error}`));
 	server.listen(listenPort, listenHost, () => console.log(`Worker ${process.pid} started and listening on ${listenHost}:${listenPort}`));
 }
