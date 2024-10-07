b16cf2fb8867efdbc86af93007d8d031cd89b497
kiki-kanri <a470666@gmail.com>
2024-10-07 23:55:40 +0800

    feat: add `requireTwoFactorAuthentication` util export to `utils/index.ts`

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export { commonAjvValidatorSchemas } from '@kikiutils/kiki-core-stack-pack/constants/validator';
 export * from '@kikiutils/kiki-core-stack-pack/nitro/utils';
+export { requireTwoFactorAuthentication } from '@kikiutils/kiki-core-stack-pack/nitro/utils/two-factor-authentication';
 export { compileH3RequestDataValidator } from '@kikiutils/kiki-core-stack-pack/nitro/utils/validator';
