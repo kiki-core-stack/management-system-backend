d083b4e05ad48811eb1e9e258f7ea6d459809728
kiki-kanri <a470666@gmail.com>
2024-10-09 00:02:26 +0800

    chore!: remove files

--- types/h3.ts
+++
@@ -1,9 +0,0 @@
-import type { H3Event } from 'h3';
-
-declare module 'h3' {
-	interface H3EventContext {}
-}
-
-declare global {
-	type H3RequestEvent = H3Event;
-}
--- types/index.ts
+++
@@ -1,4 +0,0 @@
-export type {} from '@kikiutils/kiki-core-stack-pack/nitro/types/mongoose';
-export type {} from '@kikiutils/kiki-core-stack-pack/types';
-export type {} from '@kikiutils/kiki-core-stack-pack/types/data';
-export type {} from '@kikiutils/mongoose/types';
