dccbb6822dae0315ffc11c727519364d470896d7
kiki-kanri <a470666@gmail.com>
2024-10-09 15:13:31 +0800

    feat: add `defineApiHandler` utils

--- src/globals/api-utils.ts
+++ src/globals/api-utils.ts
@@ -1,3 +1,5 @@
+import type { Context } from 'hono';
+
 declare global {
 	function createApiError<D extends object>(statusCode?: StatusCode, data?: D, message?: string): ApiError<D>;
 	function createApiError<D extends object>(statusCode?: StatusCode, message?: string, data?: D): ApiError<D>;
@@ -5,6 +7,7 @@ declare global {
 	function createApiErrorAndThrow<D extends object>(statusCode?: StatusCode, message?: string, data?: D): never;
 	function createApiSuccessResponseData<D extends object>(data?: D, message?: string): ApiResponseData<D>;
 	function createApiSuccessResponseData<D extends object>(message?: string, data?: D): ApiResponseData<D>;
+	function defineApiHandler<D extends object = {}>(handler: (ctx: Context) => ApiResponseData<D> | Promise<ApiResponseData<D>> | Promise<void> | void): any;
 }
 
 globalThis.createApiError = (statusCode: StatusCode = 500, arg1: any, arg2?: any) => new ApiError(statusCode, arg1, arg2);
@@ -17,4 +20,10 @@ globalThis.createApiSuccessResponseData = (arg1: any, arg2?: any) => {
 	return { data: arg1 || {}, message: arg2 || '成功', success: true };
 };
 
-export {};
+globalThis.defineApiHandler = function (handler) {
+	return async (ctx: Context) => {
+		const data = await handler(ctx);
+		if (!data) return ctx.json(createApiSuccessResponseData());
+		return ctx.json(data);
+	};
+};
