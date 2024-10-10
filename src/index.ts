d4478028ac8350adc4e01f16c6db51362a18bc12
kiki-kanri <a470666@gmail.com>
2024-10-10 19:36:20 +0800

    chore: set up hono logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-// Initialize Mongoose
+// Initialize Mongoose setup
 await import('@kikiutils/kiki-core-stack-pack/hono-backend/setups/mongoose');
 
 // Initialize global utilities
 await import('@kikiutils/kiki-core-stack-pack/hono-backend/globals');
 
-// Import and setup server
+// Import and set up the server
 const server = (await import('@kikiutils/kiki-core-stack-pack/hono-backend/server')).default;
 await import('@kikiutils/kiki-core-stack-pack/hono-backend/setups/server');
+
+// Begin logger setup - block content can be changed but do not remove this block
+const { useHonoLogger } = await import('@kikiutils/node/hono');
+useHonoLogger(honoApp, false);
+// End logger setup - block content can be changed but do not remove this block
 
 // Load middlewares
 await import('@/middlewares');
 
-// Begin loading API routes - do not change or remove this line
+// Begin API routes setup - block content can be changed but do not remove this block
 await import('@kikiutils/kiki-core-stack-pack/hono-backend/setups/apis');
-// End loading API routes - do not change or remove this line
+// End API routes setup - block content can be changed but do not remove this block
 
 export default server;
