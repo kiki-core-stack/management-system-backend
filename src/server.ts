f31a30d0c104f988ad3ab24790e45c63df7fde1b
kiki-kanri <a470666@gmail.com>
2024-10-12 06:12:32 +0800

    feat: set OpenAPIHono defaultHook to return 400 error response

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { jsonResponseHeaders } from '@kikiutils/kiki-core-stack-pack/hono-backend/constants/response';
+import { statusCodeToResponseTextMap } from '@kikiutils/kiki-core-stack-pack/hono-backend/constants/response/status-code-to-text-map';
 import logger from '@kikiutils/node/consola';
 import type { Serve } from 'bun';
 
@@ -10,6 +12,7 @@ const _honoApp = new OpenAPIHono({
 	defaultHook(result, ctx) {
 		if (result.success) return;
 		logger.info(result.error);
+		return ctx.text(statusCodeToResponseTextMap[400]!, 400, jsonResponseHeaders);
 	}
 });
 
