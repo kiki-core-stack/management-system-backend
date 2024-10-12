673d796b32943dd1ea8f45cab6c127d6311eda6a
kiki-kanri <a470666@gmail.com>
2024-10-12 22:58:54 +0800

    refactor: move z variable definition to common package

--- src/globals/zod-validator.ts
+++ src/globals/zod-validator.ts
@@ -1,10 +1,7 @@
 import { zValidator as _zValidator } from '@hono/zod-validator';
-import { z as _z } from 'zod';
 
 declare global {
-	var z: typeof _z;
 	var zValidator: typeof _zValidator;
 }
 
-globalThis.z = _z;
 globalThis.zValidator = _zValidator;
