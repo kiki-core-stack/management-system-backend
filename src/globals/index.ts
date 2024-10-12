b34d44a99ba473eabf192414ae1d8c74c80eaa31
kiki-kanri <a470666@gmail.com>
2024-10-12 23:18:55 +0800

    chore: update src/globals/index.ts

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,6 +1,7 @@
 import '@kikiutils/kiki-core-stack-pack/hono-backend/globals';
 // import '@kikiutils/kiki-core-stack-pack/hono-backend/globals/zod';
 // import '@kikiutils/kiki-core-stack-pack/hono-backend/globals/zod-openapi';
+// import '@kikiutils/kiki-core-stack-pack/hono-backend/globals/zod-validator';
 
 // import '@/core/globals';
 // import '@/core/globals/utils';
