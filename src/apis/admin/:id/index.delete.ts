8ab1d2ecf09ce3ef51dd262fae001cc63c60796b
kiki-kanri <a470666@gmail.com>
2024-10-12 23:33:45 +0800

    refactor: rename event parameter to ctx

--- src/apis/admin/:id/index.delete.ts
+++ src/apis/admin/:id/index.delete.ts
@@ -1,10 +1,10 @@
 import { mongooseConnections } from '@kikiutils/kiki-core-stack-pack/constants/mongoose';
 import { AdminLogModel, AdminModel } from '@kikiutils/kiki-core-stack-pack/models';
 
-export default defineApiRouteHandler(async (event) => {
+export default defineApiRouteHandler(async (ctx) => {
 	return await mongooseConnections.default!.transaction(async (session) => {
-		return await getModelDocumentByRouteIdAndDelete(event, AdminModel, { session }, async (admin) => {
-			if (admin.id === event.session.adminId) throwApiError(409, '無法刪除自己！');
+		return await getModelDocumentByRouteIdAndDelete(ctx, AdminModel, { session }, async (admin) => {
+			if (admin.id === ctx.session.adminId) throwApiError(409, '無法刪除自己！');
 			if ((await AdminModel.countDocuments()) === 1) throwApiError(409, '無法刪除最後一位管理員！');
 			await AdminLogModel.deleteMany({ admin }, { session });
 		});
