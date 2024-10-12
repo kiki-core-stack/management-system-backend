0796967705f414f0ee2f13dbc80cf240eeac127c
kiki-kanri <a470666@gmail.com>
2024-10-12 14:56:32 +0800

    feat: add `RouteHandlerProperties` interface and `RouteHandler` type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,9 @@
+import type { H } from 'hono/types';
+
 export type {} from '@kikiutils/kiki-core-stack-pack/types';
+
+interface RouteHandlerProperties {}
+
+declare global {
+	type RouteHandler = H & Partial<RouteHandlerProperties>;
+}
