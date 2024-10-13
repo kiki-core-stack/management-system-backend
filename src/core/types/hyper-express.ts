7a87060ec4cf5198c4f3b7119a0007a037e7cca9
kiki-kanri <a470666@gmail.com>
2024-10-13 20:44:21 +0800

    feat: define type for hyper-express `Request.route` property

---
+++ src/core/types/hyper-express.ts
@@ -0,0 +1,16 @@
+import type { RouteRecord, Server } from 'hyper-express';
+
+interface Route extends RouteRecord {
+	app: Server;
+	id: number;
+	max_body_length: number;
+	path: string;
+	path_parameters_key: string[];
+	streaming: object;
+}
+
+declare module 'hyper-express' {
+	interface Request {
+		route?: Route;
+	}
+}
