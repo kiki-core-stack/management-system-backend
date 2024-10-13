8ac408906d9c60e8f30208263ece0194ddce40c4
kiki-kanri <a470666@gmail.com>
2024-10-14 07:02:09 +0800

    feat: add routes scanning and registration functionality in entry point

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import logger from '@kikiutils/node/consola';
 
+import { registerRoutesFromFiles } from '@/core/route';
 import { server } from '@/server';
 
+// Scan files and register routes
+await registerRoutesFromFiles(server, `${import.meta.dirname}/apis`, '/api');
+await registerRoutesFromFiles(server, `${import.meta.dirname}/routes`, '/');
+
+// Start server
 const serverHost = process.env.SERVER_HOST || '127.0.0.1';
 const serverPort = +(process.env.SERVER_PORT || 8000);
 await server.listen(serverPort, serverHost);
