55fb6d5bc8a4e18c68d6ccc032714935c4249416
kiki-kanri <a470666@gmail.com>
2024-10-16 15:21:42 +0800

    chore: disable `apiResponseMiddleware`

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,13 @@ import '@/core/globals';
 import { sessionMiddleware } from '@/core/middlewares/session';
 import { registerRoutesFromFiles } from '@/core/route';
 import '@/globals';
-import { apiResponseMiddleware } from '@/middlewares/api-response';
 import { server } from '@/server';
 
 // Setup error handling
 setupServerErrorHandling(server);
 
 // Setup middlewares
-server.use('/api', apiResponseMiddleware(), await sessionMiddleware());
+server.use('/api', await sessionMiddleware());
 
 // Scan files and register routes
 await registerRoutesFromFiles(server, `${import.meta.dirname}/apis`, '/api');
