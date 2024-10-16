0dadd206434d91e2d49a033457ffe8a1806f55a0
kiki-kanri <a470666@gmail.com>
2024-10-17 06:05:51 +0800

    refactor: rewrite session middleware to use class for session handling

--- src/core/middlewares/session/index.ts
+++ src/core/middlewares/session/index.ts
@@ -1,27 +1,10 @@
 import type { MiddlewareHandler } from '@kikiutils/hyper-express';
 
-import { sessionStorage, sessionTokenSymbol } from './constants';
-import type { PartialRequestLocalsSession } from './types';
+import Session from './classes/session';
 
-type StoredData = [number, PartialRequestLocalsSession, ip?: string];
-
-export const sessionMiddleware = async (): Promise<MiddlewareHandler> => {
-	await import('./globals');
+export const sessionMiddleware = (): MiddlewareHandler => {
 	return async (request, response) => {
-		const token = request.headers.session;
-		let sessionData: PartialRequestLocalsSession | undefined;
-		if (token) {
-			let storedData: Nullable<StoredData> = null;
-			try {
-				storedData = await sessionStorage.getItem<StoredData>(token);
-			} catch {}
-			if (storedData && storedData[0] + 86400000 >= Date.now()) sessionData = storedData[1];
-			if (!sessionData) {
-				await sessionStorage.removeItem(token);
-				response.header('set-session', '', true);
-			} else request.locals[sessionTokenSymbol] = token;
-		}
-
-		request.locals.session = sessionData || {};
+		request.session = new Session(request, response);
+		await request.session.init();
 	};
 };
