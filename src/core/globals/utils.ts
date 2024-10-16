1c10436ba1af08f09da71d6d91437fa7d76c905a
kiki-kanri <a470666@gmail.com>
2024-10-16 10:32:15 +0800

    refactor: change binding of constants or functions to globalThis to use Object.defineProperty

--- src/core/globals/utils.ts
+++ src/core/globals/utils.ts
@@ -1,13 +1,17 @@
 import type { Request } from '@kikiutils/hyper-express';
 
 declare global {
-	function getXForwardedForHeaderFirstValue(request: Request): string | undefined;
+	const getXForwardedForHeaderFirstValue: (request: Request) => string | undefined;
 }
 
-globalThis.getXForwardedForHeaderFirstValue = (request) => {
-	const xForwardedFor = request.headers['x-forwarded-for'];
-	if (!xForwardedFor) return;
-	const firstCommaIndex = xForwardedFor.indexOf(',');
-	const value = firstCommaIndex === -1 ? xForwardedFor : xForwardedFor.substring(0, firstCommaIndex);
-	return value.trim();
-};
+Object.defineProperty(globalThis, 'getXForwardedForHeaderFirstValue', {
+	configurable: false,
+	value(request: Request) {
+		const xForwardedFor = request.headers['x-forwarded-for'];
+		if (!xForwardedFor) return;
+		const firstCommaIndex = xForwardedFor.indexOf(',');
+		const value = firstCommaIndex === -1 ? xForwardedFor : xForwardedFor.substring(0, firstCommaIndex);
+		return value.trim();
+	},
+	writable: false
+});
