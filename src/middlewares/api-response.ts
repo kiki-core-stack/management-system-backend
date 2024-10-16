47f19dc69dfe99540deeff8ca74960e7a237d538
kiki-kanri <a470666@gmail.com>
2024-10-16 14:45:07 +0800

    feat: automatically respond if send is not called in handler for API request paths

---
+++ src/middlewares/api-response.ts
@@ -0,0 +1,11 @@
+import type { MiddlewareHandler } from '@kikiutils/hyper-express';
+
+declare module '@kikiutils/hyper-express' {
+	interface Response {
+		_wrapped_request: Request;
+	}
+}
+
+export const apiResponseMiddleware = (): MiddlewareHandler => (_, response) => {
+	if (!response._wrapped_request.received) sendApiSuccessResponse(response);
+};
