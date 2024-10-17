5d67369d6700b41a5250790fb570777f50cb9ddf
kiki-kanri <a470666@gmail.com>
2024-10-17 10:20:42 +0800

    refactor: update method for passing Redis URI when creating sessionStorage

--- src/core/middlewares/session/constants.ts
+++ src/core/middlewares/session/constants.ts
@@ -1,5 +1,6 @@
-import { checkAndGetEnvValue } from '@kikiutils/node/env';
 import { createStorage, prefixStorage } from 'unstorage';
 import redisDriver from 'unstorage/drivers/redis';
 
-export const sessionStorage = prefixStorage(createStorage({ driver: redisDriver({ url: checkAndGetEnvValue('REDIS_URI') }) }), 'session');
+import configs from '@/configs';
+
+export const sessionStorage = prefixStorage(createStorage({ driver: redisDriver({ url: configs.redisUri }) }), 'session');
