13b30c8785856e509ceed89f991ae4375cda024e
kiki-kanri <a470666@gmail.com>
2024-10-20 16:29:47 +0800

    feat: add OpenAPI configuration to configs

--- src/configs.ts
+++ src/configs.ts
@@ -1,11 +1,17 @@
 import { checkAndGetEnvValue } from '@kikiutils/node/env';
+import type { OpenAPIObject } from 'openapi3-ts/oas31';
 import type { ReadonlyDeep } from 'type-fest';
 
 export type Configs = ReadonlyDeep<{
+	openAPI: Omit<OpenAPIObject, 'components' | 'paths' | 'webhooks'>;
 	redisUri: string;
 }>;
 
 export const configs: Configs = {
+	openAPI: {
+		info: { title: 'API Document', version: '0.1.0' },
+		openapi: '3.1.0'
+	},
 	redisUri: checkAndGetEnvValue('REDIS_URI')
 };
 
