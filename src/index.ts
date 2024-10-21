f89e1006fd9f4f7b6dc86591f8d83e6dbc4eff7e
kiki-kanri <a470666@gmail.com>
2024-10-21 16:50:11 +0800

    feat: add entry point

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
+import { setupHonoAppErrorHandling } from '@kikiutils/kiki-core-stack-pack/hono-backend/setups/error-handling';
+import '@kikiutils/kiki-core-stack-pack/hono-backend/setups/mongoose-model-statics';
+import type { Serve } from 'bun';
+
+import { honoApp } from '@/app';
+import '@/core/globals';
+import '@/globals';
+
+// Extend Zod with OpenAPI
+extendZodWithOpenApi(z);
+
+// Setup error handling
+setupHonoAppErrorHandling(honoApp);
+
+// Start server
+const serveConfig: Serve = {
+	fetch: honoApp.fetch,
+	hostname: process.env.SERVER_HOST || '127.0.0.1',
+	port: Number(process.env.SERVER_PORT) || 8000,
+	reusePort: true
+};
+
+export default serveConfig;
