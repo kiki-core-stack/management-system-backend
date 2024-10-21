2cefdf140084872fe01388ae5277ea0290cbd8c7
kiki-kanri <a470666@gmail.com>
2024-10-21 22:43:25 +0800

    feat: add `sessionDataEncryptionKey` field to configs and env

--- src/configs.ts
+++ src/configs.ts
@@ -5,14 +5,18 @@ import type { ReadonlyDeep } from 'type-fest';
 export type Configs = ReadonlyDeep<{
 	openAPI: Omit<OpenAPIObject, 'components' | 'paths' | 'webhooks'>;
 	redisUri: string;
+	sessionDataEncryptionKey: Buffer;
 }>;
 
+const sessionDataEncryptionKey = Buffer.from(checkAndGetEnvValue('SESSION_DATA_ENCRYPTION_KEY'), 'ascii');
+if (sessionDataEncryptionKey.length !== 32) throw new Error(`Invalid SESSION_DATA_ENCRYPTION_KEY length: expected 32 bytes (256 bits for AES-256) when parsed as ASCII, but got ${sessionDataEncryptionKey.length} bytes.`);
 export const configs: Configs = {
 	openAPI: {
 		info: { title: 'API Document', version: '0.1.0' },
 		openapi: '3.1.0'
 	},
-	redisUri: checkAndGetEnvValue('REDIS_URI')
+	redisUri: checkAndGetEnvValue('REDIS_URI'),
+	sessionDataEncryptionKey
 };
 
 export default configs;
