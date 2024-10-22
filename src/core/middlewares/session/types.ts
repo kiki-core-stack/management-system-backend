899c749d5ccd46629ee7998d30b6d4d53c813f68
kiki-kanri <a470666@gmail.com>
2024-10-22 09:44:55 +0800

    feat: add cookie and header session token handlers and related types

--- src/core/middlewares/session/types.ts
+++ src/core/middlewares/session/types.ts
@@ -1,4 +1,11 @@
+import type { Context } from 'hono';
+
 export type PartialContextSessionData = Partial<ContextSessionData>;
+export type SessionTokenHandler = Readonly<{
+	delete(ctx: Context): void;
+	get(ctx: Context): string | undefined;
+	set(ctx: Context, value: string): void;
+}>;
 
 export interface ContextSessionData {}
 
