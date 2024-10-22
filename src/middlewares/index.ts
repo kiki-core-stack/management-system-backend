58eafaf3686f7dce7f2ebd0387907bd79632878e
kiki-kanri <a470666@gmail.com>
2024-10-22 15:34:01 +0800

    feat: add api middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,6 +4,8 @@ import { honoApp } from '@/app';
 import configs from '@/configs';
 import session from '@/core/middlewares/session';
 import { cookieSessionTokenHandler } from '@/core/middlewares/session/handlers/token';
+import api from './api';
 
 useHonoLogger(honoApp);
 honoApp.use('/api/*', session(configs.sessionEncryptionKey, cookieSessionTokenHandler));
+honoApp.use('/api/*', api);
