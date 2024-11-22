11eba542c5795b47aaf7d53634b8bfe57dd91d8c
kiki-kanri <a470666@gmail.com>
2024-11-23 05:59:36 +0800

    fix: use `process.on` instead of on in `run-production-cluster.ts`

--- run-production-cluster.ts
+++ run-production-cluster.ts
@@ -1,13 +1,13 @@
 import type { Subprocess } from 'bun';
-import { env, on } from 'node:process';
+import process from 'node:process';
 
 // TODO: logger, auto restart and compile this file
 
 (() => {
-	const workersCount = Number(env.CLUSTER_WORKERS) || 4;
+	const workersCount = Number(process.env.CLUSTER_WORKERS) || 4;
 	const subprocesses: Subprocess[] = Array.from({ length: workersCount });
 	for (let i = 0; i < workersCount; i++) subprocesses[i] = Bun.spawn({ cmd: ['./index'], stdio: ['pipe', 'pipe', 'pipe'] });
 	const killAllSubprocesses = (exitCode?: NodeJS.Signals | number) => subprocesses.forEach((subprocess) => subprocess.kill(exitCode));
-	on('SIGINT', killAllSubprocesses);
-	on('exit', killAllSubprocesses);
+	process.on('SIGINT', killAllSubprocesses);
+	process.on('exit', killAllSubprocesses);
 })();
