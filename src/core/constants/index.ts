f7acdba80d4c19a95e3fafb53b7151f8b578673a
kiki-kanri <a470666@gmail.com>
2024-11-23 04:41:39 +0800

    refactor: remove `projectRootDirectoryPath` constant

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -2,5 +2,4 @@
 
 import path from 'node:path';
 
-export const projectRootDirectoryPath = path.resolve(import.meta.dirname, '../../../');
 export const projectSrcDirectoryPath = path.resolve(import.meta.dirname, '../../');
