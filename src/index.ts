cf366a058e1890b292a22974a27291729d953252
kiki-kanri <a470666@gmail.com>
2024-11-23 22:16:28 +0800

    refactor: change entrypoint to IIFE

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,39 @@ import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 import { z as zod } from '@kikiutils/kiki-core-stack-pack/constants/zod';
 import { setupHonoAppErrorHandling } from '@kikiutils/kiki-core-stack-pack/hono-backend/setups/error-handling';
 import '@kikiutils/kiki-core-stack-pack/hono-backend/setups/mongoose-model-statics';
-import type { Serve } from 'bun';
 import logger from 'consola';
 import { env, pid } from 'node:process';
 
 import { honoApp } from '@/core/app';
 import '@/configs';
 
-// Extend Zod with OpenAPI
-extendZodWithOpenApi(zod);
+(async () => {
+    // Extend Zod with OpenAPI
+    extendZodWithOpenApi(zod);
 
-// Import global constants and utilities
-await import('@/core/globals');
-await import('@/globals');
+    // Import global constants and utilities
+    await import('@/core/globals');
+    await import('@/globals');
 
-// Setup error handling
-setupHonoAppErrorHandling(honoApp);
+    // Setup error handling
+    setupHonoAppErrorHandling(honoApp);
 
-// Load middlewares
-await import('@/middlewares');
+    // Load middlewares
+    await import('@/middlewares');
 
-// Scan files and register routes
-// eslint-disable-next-line node/prefer-global/process
-await import(`@/core/routes-loader/${process.env.NODE_ENV}`);
+    // Scan files and register routes
+    // eslint-disable-next-line node/prefer-global/process
+    await import(`@/core/routes-loader/${process.env.NODE_ENV}`);
 
-// Start server
-export default {
-    fetch: honoApp.fetch,
-    hostname: env.SERVER_HOST || '127.0.0.1',
-    port: Number(env.SERVER_PORT) || 8000,
-    reusePort: true,
-} satisfies Serve;
+    // Start server
+    const server = Bun.serve({
+        fetch: honoApp.fetch,
+        hostname: env.SERVER_HOST || '127.0.0.1',
+        port: Number(env.SERVER_PORT) || 8000,
+        reusePort: true,
+    });
 
-// Log that subprocess has started
-if (Bun.argv.includes('--is-subprocess')) logger.success(`[Worker ${Bun.argv[2]} (${pid})] Started.`);
+    logger.success(`Started server http://${server.hostname}:${server.port}.`);
+    // Log that subprocess has started
+    if (Bun.argv.includes('--is-subprocess')) logger.success(`[Worker ${Bun.argv[2]} (${pid})] Started.`);
+})();
