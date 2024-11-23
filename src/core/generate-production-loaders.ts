2d4a4078cd6c49ba860d42d5a7076aa539f6ac60
kiki-kanri <a470666@gmail.com>
2024-11-24 02:09:14 +0800

    feat: add middlewares loader generation functionality in `src/core/generate-production-loaders.ts`

--- src/core/generate-production-loaders.ts
+++ src/core/generate-production-loaders.ts
@@ -3,10 +3,22 @@ import { join } from 'node:path';
 import logger from '@/core/libs/logger';
 
 import { projectSrcDirectoryPath } from './constants';
+import { getMiddlewareFilePaths } from './libs/middleware';
 import { scanDirectoryForRoutes } from './libs/router';
 
-(async () => {
-    const routesFileLines = [
+async function generateMiddlewaresLoader() {
+    const fileLines = ['export default async function () {'];
+    const startTime = performance.now();
+    logger.info('Starting to generate production middlewares...');
+    const middlewareFilePaths = await getMiddlewareFilePaths();
+    for (const filePath of middlewareFilePaths) fileLines.push(`    await import('${filePath}');`);
+    fileLines.push('}');
+    await Bun.write(join(projectSrcDirectoryPath, 'core/loaders/middlewares/production.ts'), `${fileLines.join('\n')}\n`);
+    logger.success(`Generated production ${middlewareFilePaths.length} middlewares in ${(performance.now() - startTime).toFixed(2)}ms.`);
+}
+
+async function generateRoutesLoader() {
+    const fileLines = [
         `import { honoApp } from '@/core/app';`,
         `import { loadRouteModule } from '../libs/router';`,
         'export default async function () {',
@@ -15,8 +27,10 @@ import { scanDirectoryForRoutes } from './libs/router';
     const startTime = performance.now();
     logger.info('Starting to generate production routes...');
     const scannedRoutes = await scanDirectoryForRoutes(join(projectSrcDirectoryPath, 'routes'), '/');
-    for (const { filePath, ...scannedRoute } of scannedRoutes) routesFileLines.push(`    loadRouteModule(honoApp, await import('${filePath}'), ${JSON.stringify(scannedRoute)})`);
-    routesFileLines.push('};');
-    await Bun.write(join(projectSrcDirectoryPath, 'core/loaders/routes/production.ts'), `${routesFileLines.join('\n')}\n`);
+    for (const { filePath, ...scannedRoute } of scannedRoutes) fileLines.push(`    loadRouteModule(honoApp, await import('${filePath}'), ${JSON.stringify(scannedRoute)});`);
+    fileLines.push('}');
+    await Bun.write(join(projectSrcDirectoryPath, 'core/loaders/routes/production.ts'), `${fileLines.join('\n')}\n`);
     logger.success(`Generated production ${scannedRoutes.length} routes in ${(performance.now() - startTime).toFixed(2)}ms.`);
-})();
+}
+
+(async () => await Promise.all([generateMiddlewaresLoader(), generateRoutesLoader()]))();
