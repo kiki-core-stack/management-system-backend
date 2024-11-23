fdb589583d8e971036d5758ed503658d3eb9b9e6
kiki-kanri <a470666@gmail.com>
2024-11-24 01:57:52 +0800

    refactor: remove and split the original middlewares entry point

--- src/middlewares/01.session.ts
+++ src/middlewares/01.session.ts
@@ -1,12 +1,6 @@
-import { useHonoLogger } from '@kikiutils/node/hono';
-
 import configs from '@/configs';
 import { honoApp } from '@/core/app';
 import session from '@/core/middlewares/session';
 import { cookieSessionTokenHandler } from '@/core/middlewares/session/handlers/token';
 
-import api from './api';
-
-useHonoLogger(honoApp);
 honoApp.use('/api/*', session(configs.sessionCipherKey, cookieSessionTokenHandler));
-honoApp.use('/api/*', api);
