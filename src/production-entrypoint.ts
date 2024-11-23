c6baaee8e348ece83b1d3238ad3b18be13b6b3fc
kiki-kanri <a470666@gmail.com>
2024-11-23 23:43:22 +0800

    refactor: change process-related methods in `production-entrypoint.ts` to named imports

--- src/production-entrypoint.ts
+++ src/production-entrypoint.ts
@@ -1,10 +1,10 @@
 import type { Subprocess } from 'bun';
 import logger from 'consola';
-import process from 'node:process';
+import { env, once, pid } from 'node:process';
 
 (() => {
     let isShuttingDown = false;
-    const workersCount = Number(process.env.CLUSTER_WORKERS) || 4;
+    const workersCount = Number(env.CLUSTER_WORKERS) || 4;
     const subprocesses: Subprocess[] = Array.from({ length: workersCount });
     const createAndSetSubprocess = (index: number) => {
         subprocesses[index] = Bun.spawn({
@@ -17,13 +17,13 @@ import process from 'node:process';
             ],
             onExit(_subprocess, exitCode, signalCode, error) {
                 if (error || exitCode) {
-                    logger.error(`[Worker ${index} (${process.pid})] Exited with exitCode ${exitCode} and error:`, error);
-                    if (isShuttingDown) return logger.info(`[Worker ${index} (${process.pid})] Main process is shutting down. Do not restart.`);
-                    logger.info(`[Worker ${index} (${process.pid})] Restarting in 1 second...`);
+                    logger.error(`[Worker ${index} (${pid})] Exited with exitCode ${exitCode} and error:`, error);
+                    if (isShuttingDown) return logger.info(`[Worker ${index} (${pid})] Main process is shutting down. Do not restart.`);
+                    logger.info(`[Worker ${index} (${pid})] Restarting in 1 second...`);
                     setTimeout(() => createAndSetSubprocess(index), 1000);
-                } else if (exitCode !== null) logger.info(`[Worker ${index} (${process.pid})] Exited with code: ${exitCode}.`);
-                else if (signalCode !== null) logger.info(`[Worker ${index} (${process.pid})] Exited with signal: ${signalCode}.`);
-                else logger.info(`[Worker ${index} (${process.pid})] Exited.`);
+                } else if (exitCode !== null) logger.info(`[Worker ${index} (${pid})] Exited with code: ${exitCode}.`);
+                else if (signalCode !== null) logger.info(`[Worker ${index} (${pid})] Exited with signal: ${signalCode}.`);
+                else logger.info(`[Worker ${index} (${pid})] Exited.`);
             },
             stdio: ['inherit', 'inherit', 'inherit'],
         });
@@ -46,7 +46,7 @@ import process from 'node:process';
         logger.info('[Main worker] All subprocesses terminated.');
     };
 
-    process.once('SIGINT', shutdown);
-    process.once('SIGTERM', shutdown);
-    process.once('exit', shutdown);
+    once('SIGINT', shutdown);
+    once('SIGTERM', shutdown);
+    once('exit', shutdown);
 })();
