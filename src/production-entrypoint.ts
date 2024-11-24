3566fc8233e9fbd7717fbccb9cd70a2b7997199d
kiki-kanri <a470666@gmail.com>
2024-11-25 01:49:25 +0800

    refactor: update subprocess auto-restart logic in `production-entrypoint.ts` to restart continuously unless main process exits

--- src/production-entrypoint.ts
+++ src/production-entrypoint.ts
@@ -19,14 +19,13 @@ import logger from '@/core/libs/logger';
             ],
             onExit(_subprocess, exitCode, signalCode, error) {
                 const logPrefix = workerProcesses[index]!.logPrefix;
-                if (error || exitCode) {
-                    logger.error(logPrefix, `Exited with exitCode ${exitCode} and error:`, error);
-                    if (isShuttingDown) return logger.info(logPrefix, 'Main process is shutting down. Do not restart.');
-                    logger.info(logPrefix, 'Restarting in 1 second...');
-                    setTimeout(() => !isShuttingDown && createAndSetWorker(index), 1000);
-                } else if (exitCode !== null) logger.info(logPrefix, `Exited with code: ${exitCode}.`);
-                else if (signalCode !== null) logger.info(logPrefix, `Exited with signal: ${signalCode}.`);
-                else logger.info(logPrefix, `Exited.`);
+                if (error) logger.error(logPrefix, `Exited with error:`, error);
+                else if (exitCode !== null) logger.info(logPrefix, `Exited with exitCode ${exitCode}.`);
+                else if (signalCode !== null) logger.info(logPrefix, `Exited with signalCode ${signalCode}.`);
+                else logger.info(logPrefix, 'Exited with unknown reason.');
+                if (isShuttingDown) return logger.info(logPrefix, 'Main process is shutting down. Do not restart.');
+                logger.info(logPrefix, 'Restarting in 1 second...');
+                setTimeout(() => !isShuttingDown && createAndSetWorker(index), 1000);
             },
             stdio: ['inherit', 'inherit', 'inherit'],
         });
