8ae6ea7e57b2c0070330636fcde335df9e1cb0e1
kiki-kanri <a470666@gmail.com>
2024-11-24 21:23:15 +0800

    refactor: merge zod openapi `defaultAPISuccessResponseContent` component into `defaultAPISuccess` component

--- src/initializations/register-zod-openapi-components.ts
+++ src/initializations/register-zod-openapi-components.ts
@@ -1,12 +1,14 @@
 import { zodOpenAPIRegistry } from '@/core/constants/zod-openapi';
 
-zodOpenAPIRegistry.register('defaultAPISuccessResponseContent', z.object({
-    data: z.object({}).optional(),
-    message: z.string().describe('成功'),
-    success: z.literal(true),
-}));
-
 zodOpenAPIRegistry.registerComponent('responses', 'defaultAPISuccess', {
-    content: { 'application/json': { schema: { $ref: '#/components/schemas/defaultAPISuccessResponseContent' } } },
+    content: {
+        'application/json': {
+            schema: zodSchemaToOpenAPISchema(z.object({
+                data: z.object({}).optional(),
+                message: z.string().describe('成功'),
+                success: z.literal(true),
+            })),
+        },
+    },
     description: '成功',
 });
