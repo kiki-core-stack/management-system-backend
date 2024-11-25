3025249c9b9e9707386479b5b1cb25fce0396cb9
kiki-kanri <a470666@gmail.com>
2024-11-25 16:01:55 +0800

    feat: update logger methods parameters to accept types consistent with consola

--- src/core/utils/logger.ts
+++ src/core/utils/logger.ts
@@ -1,4 +1,5 @@
 import { createConsola } from 'consola';
+import type { InputLogObject } from 'consola';
 import { colorize } from 'consola/utils';
 import { format } from 'date-fns';
 import { pid } from 'node:process';
@@ -7,10 +8,10 @@ const consola = createConsola({ formatOptions: { date: false } });
 const createLogDateTimePrefix = () => `[${format(new Date(), 'yyyy-MM-dd HH:mm:ss.SSS')}]`;
 const logPrefix = Bun.argv.includes('--is-subprocess') ? colorize('cyan', `[Worker ${Bun.argv[2]} (${pid})]`) : colorize('green', '[Main worker]');
 export const logger = {
-    error: (message: string, ...args: any[]) => consola.error(createLogDateTimePrefix(), logPrefix, message, ...args),
-    info: (message: string, ...args: any[]) => consola.info(createLogDateTimePrefix(), logPrefix, message, ...args),
-    success: (message: string, ...args: any[]) => consola.success(createLogDateTimePrefix(), logPrefix, message, ...args),
-    warn: (message: string, ...args: any[]) => consola.warn(createLogDateTimePrefix(), logPrefix, message, ...args),
+    error: (message: InputLogObject | any, ...args: any[]) => consola.error(createLogDateTimePrefix(), logPrefix, message, ...args),
+    info: (message: InputLogObject | any, ...args: any[]) => consola.info(createLogDateTimePrefix(), logPrefix, message, ...args),
+    success: (message: InputLogObject | any, ...args: any[]) => consola.success(createLogDateTimePrefix(), logPrefix, message, ...args),
+    warn: (message: InputLogObject | any, ...args: any[]) => consola.warn(createLogDateTimePrefix(), logPrefix, message, ...args),
 };
 
 export default logger;
