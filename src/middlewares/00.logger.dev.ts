31270af458e87de9b750b7e0bbd872018d3a6a57
kiki-kanri <a470666@gmail.com>
2025-01-01 13:16:48 +0800

    refactor: modify logger middleware to remove `useHonoLogger` usage

--- src/middlewares/00.logger.dev.ts
+++ src/middlewares/00.logger.dev.ts
@@ -1,5 +1,16 @@
-import { useHonoLogger } from '@kikiutils/node/hono';
+import { logger as honoLogger } from 'hono/logger';
+import pino from 'pino';
+import pinoPretty from 'pino-pretty';
 
 import { honoApp } from '@/core/app';
 
-useHonoLogger(honoApp);
+const pinoLogger = pino(
+    {},
+    pinoPretty({
+        colorize: true,
+        ignore: 'hostname,pid',
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l',
+    }),
+);
+
+honoApp.use(honoLogger((text) => text[0] !== '<' && pinoLogger.info(text.substring(4))));
