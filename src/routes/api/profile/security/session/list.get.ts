a4c4ef61337d988eafc9c64e63e704535c0df518
kiki-kanri <a470666@gmail.com>
2025-03-23 06:24:53 +0800

    feat: add APIs to get the list of current logged-in sessions and delete a specified session

---
+++ src/routes/api/profile/security/session/list.get.ts
@@ -0,0 +1,19 @@
+import { AdminSessionModel } from '@kiki-core-stack/pack/models/admin/session';
+
+import { getAuthToken } from '@/libs/auth';
+
+export default defaultHonoFactory.createHandlers(async (ctx) => {
+    const adminSessions = await AdminSessionModel.find({ admin: ctx.adminId }).sort({ lastActiveAt: -1 });
+    const token = getAuthToken(ctx);
+    const adminSessionList = [];
+    for (const adminSession of adminSessions) {
+        if (adminSession.token === token) adminSessionList.unshift(adminSession.toJSON());
+        else adminSessionList.push(adminSession.toJSON());
+    }
+
+    adminSessionList[0]!.isCurrent = true;
+    return ctx.createApiSuccessResponse({
+        count: adminSessionList.length,
+        list: adminSessionList,
+    });
+});
