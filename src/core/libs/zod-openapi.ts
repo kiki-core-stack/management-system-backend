5efc534e93a341463cd02386896ec0582ff07db9
kiki-kanri <a470666@gmail.com>
2025-03-31 16:05:32 +0800

    feat: add `numberEnumToZodOpenApiSchema` core libs

--- src/core/libs/zod-openapi.ts
+++ src/core/libs/zod-openapi.ts
@@ -3,9 +3,14 @@ import {
     OpenAPIRegistry,
 } from '@asteasolutions/zod-to-openapi';
 import type { RouteConfig } from '@asteasolutions/zod-to-openapi';
+import { getEnumNumberValues } from '@kikiutils/node/enum';
 import type { SchemaObject } from 'openapi3-ts/oas31';
 import type { Except } from 'type-fest';
-import type { z } from 'zod';
+import { z } from 'zod';
+import type {
+    EnumLike,
+    ZodNativeEnum,
+} from 'zod';
 
 export type RouteZodOpenApiConfig = Except<RouteConfig, 'method' | 'path'>;
 
@@ -19,6 +24,22 @@ export function defineRouteZodOpenApiConfig(
         description,
         operationId,
     };
+}
+
+export function numberEnumToZodOpenApiSchema<T extends EnumLike>(
+    enumName: string,
+    enumObject: T,
+    toTextMap?: Record<number | string, string>,
+): ZodNativeEnum<T> {
+    return z.nativeEnum(enumObject).openapi(
+        enumName,
+        {
+            'x-enum-descriptions': toTextMap
+                ? getEnumNumberValues(enumObject).map((key: number) => toTextMap[key])
+                : undefined,
+            'x-enum-varnames': Object.keys(enumObject).filter((key) => !Number.isFinite(+key)),
+        },
+    );
 }
 
 export function zodSchemaToOpenApiSchema(schema: ReturnType<(typeof z)['object']>, description?: string): SchemaObject {
