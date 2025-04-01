b42ca39ca40ccdf181a397ab2e637f624eea57c5
kiki-kanri <a470666@gmail.com>
2025-04-01 12:08:04 +0800

    refactor: simplify `ctx.createApiSuccessResponse` method
    - fix parameter order

--- src/middlewares/10.api.ts
+++ src/middlewares/10.api.ts
@@ -5,16 +5,12 @@ import { honoApp } from '@/core/app';
 
 declare module 'hono' {
     interface Context {
-        createApiSuccessResponse: {
-            (): Response;
-            <D extends object>(data: D, message?: string): Response;
-            <D extends object>(message: string, data?: D): Response;
-        };
+        createApiSuccessResponse: <D extends object | undefined = undefined>(data?: D, message?: string) => Response;
     }
 }
 
-function createApiSuccessResponse(this: Context, arg1?: any, arg2?: any) {
-    return this.json(createApiSuccessResponseData(arg1, arg2));
+function createApiSuccessResponse<D extends object | undefined = undefined>(this: Context, data?: D, message?: string) {
+    return this.json(createApiSuccessResponseData(data, message));
 }
 
 honoApp.use('/api/*', (ctx, next) => {
