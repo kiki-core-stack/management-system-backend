8f6ae64cf82356d4105022b6458bcac490a8571b
kiki-kanri <a470666@gmail.com>
2025-04-06 16:05:04 +0800

    refactor: update `getProcessedApiRequestQueries`
    - rename internal variables
    - align with `ProcessedApiRequestQueries` field changes
    - handle fields with IIFE, add try-catch, and ensure it's an array

--- src/libs/request.ts
+++ src/libs/request.ts
@@ -22,35 +22,34 @@ export function getProcessedApiRequestQueries(
     filterInFields?: Dict<string>,
     processObjectIdIgnoreFields?: string[],
 ): ProcessedApiRequestQueries {
-    const filterQuery: Dict<any> = {};
-    let selectFields: Nullable<Set<string>> = null;
+    const filter: Dict<any> = {};
     const queries: {
-        filterQuery?: string;
+        fields?: string;
+        filter?: string;
         limit?: string;
         page?: string;
-        selectFields?: string;
     } = ctx.req.query();
 
     // TODO: 重構與資料驗證並移除不該出現的搜尋條件
-    if (queries.filterQuery) {
-        let filterQueryData = JSON.parse(queries.filterQuery);
-        if (filterQueryData.endAt) merge(filterQuery, { createdAt: { $lt: new Date(filterQueryData.endAt) } });
-        if (filterQueryData.startAt) merge(filterQuery, { createdAt: { $gte: new Date(filterQueryData.startAt) } });
+    if (queries.filter) {
+        let filterData = JSON.parse(queries.filter);
+        if (filterData.endAt) merge(filter, { createdAt: { $lt: new Date(filterData.endAt) } });
+        if (filterData.startAt) merge(filter, { createdAt: { $gte: new Date(filterData.startAt) } });
         // eslint-disable-next-line style/array-bracket-newline, style/array-element-newline
-        Object.entries(filterQueryData = omit(filterQueryData, 'endAt', 'startAt')).forEach(([key, value]) => {
-            if (key.endsWith('Id') && !processObjectIdIgnoreFields?.includes(key) && delete filterQueryData[key]) {
-                filterQuery[key.slice(0, -2)] = convertToObjectIdIfValid(value);
+        Object.entries(filterData = omit(filterData, 'endAt', 'startAt')).forEach(([key, value]) => {
+            if (key.endsWith('Id') && !processObjectIdIgnoreFields?.includes(key) && delete filterData[key]) {
+                filter[key.slice(0, -2)] = convertToObjectIdIfValid(value);
             }
 
             if (
                 key.endsWith('Ids')
                 && !filterInFields?.[key]
-                && delete filterQueryData[key]
+                && delete filterData[key]
                 && Array.isArray(value) && value.length
-            ) merge(filterQuery, { [key.slice(0, -3)]: { $in: convertToObjectIdArray(value) } });
+            ) merge(filter, { [key.slice(0, -3)]: { $in: convertToObjectIdArray(value) } });
 
-            if (value?.$regex !== undefined && delete filterQueryData[key] && value.$regex) {
-                filterQuery[key] = (value.$regex = processRegexString(value.$regex), value);
+            if (value?.$regex !== undefined && delete filterData[key] && value.$regex) {
+                filter[key] = (value.$regex = processRegexString(value.$regex), value);
             }
 
             // eslint-disable-next-line style/max-len
@@ -58,26 +57,32 @@ export function getProcessedApiRequestQueries(
             Object.entries({ ...baseFilterInFields, ...filterInFields }).forEach(([toCheckField, filterField]) => {
                 if (
                     key === toCheckField
-                    && delete filterQueryData[toCheckField]
+                    && delete filterData[toCheckField]
                     && Array.isArray(value)
                     && value.length
-                ) merge(filterQuery, { [filterField]: { $in: convertToObjectIdArray(value) } });
+                ) merge(filter, { [filterField]: { $in: convertToObjectIdArray(value) } });
             });
         });
 
-        merge(filterQuery, filterQueryData);
+        merge(filter, filterData);
     }
 
-    if (queries.selectFields) selectFields = new Set(JSON.parse(queries.selectFields));
     const limit = Math.min(Math.abs(Number(queries.limit) || 10), 1000);
     const page = Math.abs(Number(queries.page) || 1);
     const offset = limit * page;
     return {
-        filterQuery,
+        fields: (() => {
+            try {
+                const fields = JSON.parse(queries.fields || '[]');
+                return Array.isArray(fields) ? fields : [];
+            } catch {
+                return [];
+            }
+        })(),
+        filter,
         limit,
         offset,
         page,
-        selectFields: [...selectFields || []],
         skip: (page - 1) * limit,
     };
 }
