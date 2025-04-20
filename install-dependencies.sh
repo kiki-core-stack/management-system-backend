b507fb358773d4687d49ac52a619a5baf1d37b17
kiki-kanri <a470666@gmail.com>
2025-04-20 21:27:18 +0800

    chore: update all scripts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -e
+
 . ./.env.development.local
 [ -z "$NPM_CONFIG_REGISTRY" ] && NPM_CONFIG_REGISTRY='https://registry.npmjs.org'
 export NPM_CONFIG_REGISTRY
+
 bun i
+./modify-files-permissions.sh
--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set -e
+
 . ./.env.development.local
 [ -z "$NPM_CONFIG_REGISTRY" ] && NPM_CONFIG_REGISTRY='https://registry.npmjs.org'
 export NPM_CONFIG_REGISTRY
+
 pnpm upgrade -L --lockfile-only
 bun i
 bun update
