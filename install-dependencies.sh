d2355ce412618cd320daa6585d629bd298d55140
kiki-kanri <a470666@gmail.com>
2025-04-20 21:31:32 +0800

    chore: update process dependencies scripts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -e
-
 . ./.env.development.local
-[ -z "$NPM_CONFIG_REGISTRY" ] && NPM_CONFIG_REGISTRY='https://registry.npmjs.org'
 export NPM_CONFIG_REGISTRY
+
+set -e
 
 bun i
 ./modify-files-permissions.sh
