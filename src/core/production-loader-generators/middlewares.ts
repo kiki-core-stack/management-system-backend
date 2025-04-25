0c17ce5d5ebf547877e31de5386d4b4ca5061aad
kiki-kanri <a470666@gmail.com>
2025-04-25 10:59:42 +0800

    refactor: update production middleware loader generator
    - rename variables
    - update log messages
    - replace for loop with `forEach`
    - use `Bun.write` for file output
    - change `export *` to `export {}`

--- src/core/production-loader-generators/middlewares.ts
+++ src/core/production-loader-generators/middlewares.ts
@@ -1,21 +1,17 @@
-import { writeFile } from 'node:fs/promises';
-
 import { productionMiddlewaresLoaderPath } from '../constants/paths';
 import { getMiddlewareFilePaths } from '../libs/middleware';
 import { logger } from '../utils/logger';
 
-const fileLines = [
-    '// @ts-nocheck',
+const outputLines = [
+    '// @ts-nocheck', // Suppress type checking for generated file
     '',
 ];
 
 const startTime = performance.now();
-logger.info('Starting to generate production middlewares...');
-const middlewareFilePaths = await getMiddlewareFilePaths();
-for (let i = 0; i < middlewareFilePaths.length; i++) {
-    fileLines.push(`export * as middleware${i} from '${middlewareFilePaths[i]}';`);
-}
-
-await writeFile(productionMiddlewaresLoaderPath, `${fileLines.join('\n')}\n`);
-// eslint-disable-next-line style/max-len
-logger.success(`Generated production ${middlewareFilePaths.length} middlewares in ${(performance.now() - startTime).toFixed(2)}ms.`);
+logger.info('Generating production middleware loader...');
+const middlewarePaths = await getMiddlewareFilePaths();
+middlewarePaths.forEach((path) => outputLines.push(`export {} from '${path}';`));
+await Bun.write(productionMiddlewaresLoaderPath, `${outputLines.join('\n')}\n`);
+logger.success(
+    `Generated ${middlewarePaths.length} production middlewares in ${(performance.now() - startTime).toFixed(2)}ms.`,
+);
