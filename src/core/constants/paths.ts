826826ea0a184cf856d8c2dc69399d542c9af855
kiki-kanri <a470666@gmail.com>
2025-04-25 12:48:01 +0800

    chore: add `/* @__PURE__ */` annotation to `src/core/constants/paths.ts` to avoid inclusion in bundle output

--- src/core/constants/paths.ts
+++ src/core/constants/paths.ts
@@ -8,9 +8,10 @@ import {
     sep,
 } from 'node:path';
 
-export const projectRoot = resolve(import.meta.dirname, '../../../').replaceAll(sep, '/');
-export const projectSrcDirPath = join(projectRoot, 'src');
-export const middlewaresDirPath = join(projectSrcDirPath, 'middlewares');
-export const productionMiddlewaresLoaderPath = join(projectSrcDirPath, 'core/loaders/middlewares/production.ts');
-export const productionRoutesLoaderPath = join(projectSrcDirPath, 'core/loaders/routes/production.ts');
-export const routesDirPath = join(projectSrcDirPath, 'routes');
+export const projectRoot = /* @__PURE__ */ resolve(import.meta.dirname, '../../../').replaceAll(sep, '/');
+export const projectSrcDirPath = /* @__PURE__ */ join(projectRoot, 'src');
+export const middlewaresDirPath = /* @__PURE__ */ join(projectSrcDirPath, 'middlewares');
+// eslint-disable-next-line style/max-len
+export const productionMiddlewaresLoaderPath = /* @__PURE__ */ join(projectSrcDirPath, 'core/loaders/middlewares/production.ts');
+export const productionRoutesLoaderPath = /* @__PURE__ */ join(projectSrcDirPath, 'core/loaders/routes/production.ts');
+export const routesDirPath = /* @__PURE__ */ join(projectSrcDirPath, 'routes');
