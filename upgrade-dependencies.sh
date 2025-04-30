cda88394d74b8d61bef34baac87e4ef23fc8192e
kiki-kanri <a470666@gmail.com>
2025-05-01 02:29:28 +0800

    chore: update various files
    - update `.dockerignore`
    - remove `set -e` and format code in some scripts
    - ensure all scripts change to their own directory before execution

--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd "$(realpath "$(dirname "$(readlink -f "$0")")")"
+cd "$(realpath "$(dirname "$(readlink -f "$0")")")" || exit 1
+
 . ./.env.development.local
 export NPM_CONFIG_REGISTRY
 
-set -e
-
 [[ " $@ " =~ ' -c ' ]] && rm -rf ./bun.lock ./node_modules ./pnpm-lock.yaml
 
-pnpm upgrade -L --lockfile-only
-bun i
-bun update
+pnpm upgrade -L --lockfile-only &&
+    bun i &&
+    bun update
+
 ./modify-files-permissions.sh
