701810aaf74be280c6b1406367d466e5a0cc0780
kiki-kanri <a470666@gmail.com>
2025-05-15 16:16:35 +0800

    refactor: update `src/routes/api/profile/index.get.ts` API to return early if `ctx.adminId` is missing

--- src/routes/api/profile/index.get.ts
+++ src/routes/api/profile/index.get.ts
@@ -4,6 +4,6 @@ import { defineRouteHandlerOptions } from '@/core/libs/route';
 export const routeHandlerOptions = defineRouteHandlerOptions({ properties: { noLoginRequired: true } });
 
 export default defaultHonoFactory.createHandlers((ctx) => {
-    if (ctx.adminId) return ctx.createApiSuccessResponse({ id: ctx.adminId });
-    return ctx.createApiSuccessResponse();
+    if (!ctx.adminId) return ctx.createApiSuccessResponse();
+    return ctx.createApiSuccessResponse({ id: ctx.adminId });
 });
