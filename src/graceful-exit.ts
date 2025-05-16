d039c3332dab8380e86fc88c1da5c7a477aa7386
kiki-kanri <a470666@gmail.com>
2025-05-16 15:33:46 +0800

    refactor: use `process.on` instead of `process.once` for `gracefulExit` and prevent duplicate execution with `isGracefulExitStarted`

--- src/graceful-exit.ts
+++ src/graceful-exit.ts
@@ -2,7 +2,11 @@ import type { Server } from 'bun';
 
 import { logger } from '@/core/utils/logger';
 
+let isGracefulExitStarted = false;
+
 export async function gracefulExit(server?: Server) {
+    if (isGracefulExitStarted) return;
+    isGracefulExitStarted = true;
     logger.info('Graceful shutdown started.');
     await server?.stop(true);
     // Perform operations such as closing the database connection here.
