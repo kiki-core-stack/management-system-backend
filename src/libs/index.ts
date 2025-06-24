45a9a66c7b1d82a8abc181bad19b8da2bd2bc373
kiki-kanri <a470666@gmail.com>
2025-06-24 11:02:31 +0800

    feat: add `assertNotModifiedAndStripData` lib

---
+++ src/libs/index.ts
@@ -0,0 +1,10 @@
+import { throwApiError } from '@kiki-core-stack/pack/hono-backend/libs/api';
+
+export function assertNotModifiedAndStripData<T extends { updatedAt: string }>(
+    payload: T,
+    document: { updatedAt: Date },
+) {
+    const { updatedAt, ...data } = payload;
+    if (updatedAt !== document.updatedAt.toISOString()) throwApiError(409, '該資料已被修改！請重新整理。');
+    return data;
+}
--- src/routes/api/admin/[id]/index.put.ts
+++ src/routes/api/admin/[id]/index.put.ts
@@ -1,4 +1,3 @@
-import { throwApiError } from '@kiki-core-stack/pack/hono-backend/libs/api';
 import { apiZValidator } from '@kiki-core-stack/pack/hono-backend/libs/api/zod-validator';
 import * as z from '@kiki-core-stack/pack/libs/zod';
 import { AdminModel } from '@kiki-core-stack/pack/models/admin';
@@ -9,6 +8,7 @@ import { assertMongooseUpdateSuccess } from '@kikiutils/mongoose/utils';
 import type { UpdateQuery } from 'mongoose';
 
 import { defaultHonoFactory } from '@/core/constants/hono';
+import { assertNotModifiedAndStripData } from '@/libs';
 
 import { jsonSchema } from '../index.post';
 
@@ -16,9 +16,7 @@ export default defaultHonoFactory.createHandlers(
     apiZValidator('json', jsonSchema.extend({ updatedAt: z.strictIsoDateString() })),
     async (ctx) => {
         const admin = await AdminModel.findByRouteIdOrThrowNotFoundError(ctx);
-        const { updatedAt, ...data } = ctx.req.valid('json');
-        if (updatedAt !== admin.updatedAt.toISOString()) throwApiError(409, '該資料已被修改！請重新整理。');
-        const updateQuery: UpdateQuery<Admin> = data;
+        const updateQuery: UpdateQuery<Admin> = assertNotModifiedAndStripData(ctx.req.valid('json'), admin);
         updateQuery.enabled = updateQuery.enabled || admin._id.equals(ctx.adminId);
         if (!updateQuery.email) updateQuery.$unset = { email: true };
         return await mongooseConnections.default!.transaction(async (session) => {
