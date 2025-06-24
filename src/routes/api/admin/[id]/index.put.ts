b8d042bd6ee2b4b500f29172983b8f9fe82162a5
kiki-kanri <a470666@gmail.com>
2025-06-24 10:28:21 +0800

    feat: add optimistic locking to admin API using `updatedAt` field

--- src/routes/api/admin/[id]/index.put.ts
+++ src/routes/api/admin/[id]/index.put.ts
@@ -1,4 +1,6 @@
+import { throwApiError } from '@kiki-core-stack/pack/hono-backend/libs/api';
 import { apiZValidator } from '@kiki-core-stack/pack/hono-backend/libs/api/zod-validator';
+import * as z from '@kiki-core-stack/pack/libs/zod';
 import { AdminModel } from '@kiki-core-stack/pack/models/admin';
 import type { Admin } from '@kiki-core-stack/pack/models/admin';
 import { AdminSessionModel } from '@kiki-core-stack/pack/models/admin/session';
@@ -11,10 +13,12 @@ import { defaultHonoFactory } from '@/core/constants/hono';
 import { jsonSchema } from '../index.post';
 
 export default defaultHonoFactory.createHandlers(
-    apiZValidator('json', jsonSchema),
+    apiZValidator('json', jsonSchema.extend({ updatedAt: z.strictIsoDateString() })),
     async (ctx) => {
         const admin = await AdminModel.findByRouteIdOrThrowNotFoundError(ctx);
-        const updateQuery: UpdateQuery<Admin> = ctx.req.valid('json');
+        const { updatedAt, ...data } = ctx.req.valid('json');
+        if (updatedAt !== admin.updatedAt.toISOString()) throwApiError(409, '該資料已被修改！請重新整理。');
+        const updateQuery: UpdateQuery<Admin> = data;
         updateQuery.enabled = updateQuery.enabled || admin._id.equals(ctx.adminId);
         if (!updateQuery.email) updateQuery.$unset = { email: true };
         return await mongooseConnections.default!.transaction(async (session) => {
