86e37eadb0e3a136ef1bc81dc5a4d67e509de8a6
kiki-kanri <a470666@gmail.com>
2025-07-16 15:31:51 +0800

    refactor: update `ZodValidatorType` to use `DocType` instead of `Data` as the input type parameter

--- src/routes/api/admin/index.post.ts
+++ src/routes/api/admin/index.post.ts
@@ -1,16 +1,16 @@
 import { apiZValidator } from '@kiki-core-stack/pack/hono-backend/libs/api/zod-validator';
 import * as z from '@kiki-core-stack/pack/libs/zod';
 import { AdminModel } from '@kiki-core-stack/pack/models/admin';
-import type { AdminData } from '@kiki-core-stack/pack/types/data/admin';
+import type { Admin } from '@kiki-core-stack/pack/models/admin';
 
 import { defaultHonoFactory } from '@/core/constants/hono';
 
 export const jsonSchema = z.object({
     account: z.string().trim().min(1).max(16),
-    email: z.string().trim().email().toLowerCase().optional(),
+    email: z.email().trim().toLowerCase().optional(),
     enabled: z.boolean(),
     password: z.string().trim().length(128).optional(),
-}) satisfies ZodValidatorType<AdminData>;
+}) satisfies ZodValidatorType<Admin>;
 
 export default defaultHonoFactory.createHandlers(
     apiZValidator('json', jsonSchema),
