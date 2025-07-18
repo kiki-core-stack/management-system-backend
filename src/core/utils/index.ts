a3121d0b498ec9dfbfa77e7bea8dab9d08b09cfa
kiki-kanri <a470666@gmail.com>
2025-07-18 13:20:07 +0800

    refactor: remove `getXForwardedForHeaderFirstValue`

--- src/core/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Context } from 'hono';
-
-export function getXForwardedForHeaderFirstValue(ctx: Context) {
-    const xForwardedFor = ctx.req.header('x-forwarded-for');
-    if (!xForwardedFor) return;
-    const firstCommaIndex = xForwardedFor.indexOf(',');
-    return (firstCommaIndex === -1 ? xForwardedFor : xForwardedFor.substring(0, firstCommaIndex)).trim();
-}
