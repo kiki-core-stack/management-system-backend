1ff59103ded47b128fcb77cacba760137ca34d93
kiki-kanri <a470666@gmail.com>
2025-07-18 11:28:28 +0800

    refactor!: remove `numberEnumToZodOpenApiSchema`

--- src/core/libs/zod-openapi.ts
+++ src/core/libs/zod-openapi.ts
@@ -3,41 +3,13 @@ import {
     OpenAPIRegistry,
 } from '@asteasolutions/zod-to-openapi';
 import type { RouteConfig } from '@asteasolutions/zod-to-openapi';
-import { getEnumNumberValues } from '@kikiutils/shared/enum';
 import type { SchemaObject } from 'openapi3-ts/oas31';
 import type { Except } from 'type-fest';
-import * as z from 'zod';
-import type {
-    core,
-    ZodObject,
-} from 'zod';
+import type { ZodObject } from 'zod';
 
 export type RouteZodOpenApiConfig = Except<RouteConfig, 'method' | 'path'>;
 
 export const defineRouteZodOpenApiConfig = (config: RouteZodOpenApiConfig): RouteZodOpenApiConfig => config;
-
-export function numberEnumToZodOpenApiSchema<T extends core.util.EnumLike>(
-    enumName: string,
-    enumObject: T,
-    toTextMap?: Record<number | string, string>,
-) {
-    const schema = z.preprocess(
-        (value) => typeof value === 'number' || typeof value === 'string' ? +value : value,
-        z.enum(enumObject),
-    );
-
-    // Remove the next line if you need OpenAPI metadata in production.
-    if (process.env.NODE_ENV === 'production') return schema;
-    return schema.openapi(
-        enumName,
-        {
-            'x-enum-descriptions': toTextMap
-                ? getEnumNumberValues(enumObject).map((key: number) => toTextMap[key])
-                : undefined,
-            'x-enum-varnames': Object.keys(enumObject).filter((key) => !Number.isFinite(+key)),
-        },
-    );
-}
 
 export function zodSchemaToOpenApiSchema(schema: ZodObject, description?: string): SchemaObject {
     const registry = new OpenAPIRegistry();
