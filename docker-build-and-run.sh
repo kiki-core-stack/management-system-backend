2c2488b4a0a2ca25e134a1b3f59c4ac88c42bd73
kiki-kanri <a470666@gmail.com>
2025-07-24 17:35:07 +0800

    chore: update `docker-build-and-run.sh` to split docker image tag into name and tag and modify example env accordingly

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -12,8 +12,9 @@ cd "${SCRIPT_DIR}"
 docker pull oven/bun:slim
 
 # Build and run
+DOCKER_IMAGE_REF="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG:-latest}"
 docker build \
-    -t "${DOCKER_IMAGE_TAG}" \
+    -t "${DOCKER_IMAGE_REF}" \
     --build-arg "NPM_REGISTRY=${NPM_REGISTRY}" \
     .
 
@@ -23,4 +24,4 @@ docker run \
     -d \
     --name "${DOCKER_CONTAINER_NAME}" \
     --restart=always \
-    "${DOCKER_IMAGE_TAG}"
+    "${DOCKER_IMAGE_REF}"
