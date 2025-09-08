5597771a2cf17710560eb31b3ab51fee24bd70ef
kiki-kanri <a470666@gmail.com>
2025-09-08 13:38:11 +0800

    refactor: remove unnecessary type assertions

--- src/routes/api/admin/[id]/boolean-field.patch.ts
+++ src/routes/api/admin/[id]/boolean-field.patch.ts
@@ -3,10 +3,7 @@ import { AdminModel } from '@kiki-core-stack/pack/models/admin';
 import type { Admin } from '@kiki-core-stack/pack/models/admin';
 import { AdminSessionModel } from '@kiki-core-stack/pack/models/admin/session';
 import { mongooseConnections } from '@kikiutils/mongoose/constants';
-import type {
-    FilterQuery,
-    Types,
-} from 'mongoose';
+import type { FilterQuery } from 'mongoose';
 
 import { defaultHonoFactory } from '@/core/constants/hono';
 import { getAdminPermission } from '@/libs/admin/permission';
@@ -26,8 +23,7 @@ export default defaultHonoFactory.createHandlers(async (ctx) => {
             { session },
             async (admin, field) => {
                 if (field === 'enabled') {
-                    // TODO 解決unknown
-                    if ((admin._id as Types.ObjectId).equals(ctx.adminId)) throwApiError(400, '無法變更自己的啟用狀態');
+                    if (admin._id.equals(ctx.adminId)) throwApiError(400, '無法變更自己的啟用狀態');
                     await AdminSessionModel.deleteMany({ admin }, { session });
                 }
             },
