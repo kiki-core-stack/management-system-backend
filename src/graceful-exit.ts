dd74429ce96c7f30cb21d6dc45a57836b3f89c42
kiki-kanri <a470666@gmail.com>
2025-09-18 11:15:21 +0800

    style: add blank lines in `gracefulExit` to separate code blocks

--- src/graceful-exit.ts
+++ src/graceful-exit.ts
@@ -10,6 +10,7 @@ export async function gracefulExit(server?: Server) {
     logger.info('Starting graceful shutdown...');
     await server?.stop(true);
     // Perform operations such as closing the database connection here.
+
     logger.success('Graceful shutdown completed');
     process.exit(0);
 }
