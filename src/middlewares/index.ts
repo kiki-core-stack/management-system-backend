e233e495c0a5a8e19d34d0021cd61a8f16079619
kiki-kanri <a470666@gmail.com>
2025-09-18 11:47:16 +0800

    refactor(middleware): rename middleware files and configure manual loading

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,6 @@
 // Control the middleware registration condition and order here
-if (process.env.NODE_ENV === 'development') await import('@/middlewares/logger');
+if (process.env.NODE_ENV === 'development') await import('./logger');
+await import('./route');
+await import('./replay-protection');
+await import('./session');
+await import('./api');
