cf6d26b7ce1fa32043652bcc7d80a918ed2b32fe
kiki-kanri <a470666@gmail.com>
2025-09-23 10:51:55 +0800

    feat: inject related info when fetching admin permission list

--- src/routes/api/admin/admin/role/list.get.ts
+++ src/routes/api/admin/admin/role/list.get.ts
@@ -1,8 +1,16 @@
 import { AdminRoleModel } from '@kiki-core-stack/pack/models/admin/role';
 
+import { populateCreatedAndEditedByAdminOptions } from '@/constants/admin';
 import { defaultHonoFactory } from '@/core/constants/hono';
 import { paginateModelDataWithApiResponse } from '@/libs/response';
 
 export const routePermission = 'admin admin.role.list';
 
-export default defaultHonoFactory.createHandlers((ctx) => paginateModelDataWithApiResponse(ctx, AdminRoleModel));
+export default defaultHonoFactory.createHandlers((ctx) => {
+    return paginateModelDataWithApiResponse(
+        ctx,
+        AdminRoleModel,
+        undefined,
+        { populate: populateCreatedAndEditedByAdminOptions },
+    );
+});
