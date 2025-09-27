603bbe37998e682cd23f1cea64b9e1e13de44a95
kiki-kanri <a470666@gmail.com>
2025-09-28 04:33:18 +0800

    refactor: simplify roles field definition in admin API validator

--- src/routes/api/admin/admin/index.post.ts
+++ src/routes/api/admin/admin/index.post.ts
@@ -13,7 +13,7 @@ export const jsonSchema = z.object({
     email: z.email().trim().toLowerCase().optional(),
     enabled: z.boolean(),
     password: z.string().trim().min(1).optional(),
-    roles: z.array(z.objectId().refine(async (id) => await AdminRoleModel.exists({ _id: id }) !== null)),
+    roles: z.array(z.objectId().refine((_id) => AdminRoleModel.exists({ _id }))),
 }) satisfies ZodValidatorType<Admin, 'isSuperAdmin'>;
 
 export const routePermission = 'admin admin.create';
